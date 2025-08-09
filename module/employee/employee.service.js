d57f31a2f8c60e591f5c24ce46c2d4f29d55a7f1
lockwud <oklement3@gmail.com>
2025-08-09 17:41:30 +0000

    UPDATE: Adding the employee registration route

---
+++ module/employee/employee.service.js
@@ -0,0 +1,21 @@
+const connection = require("../../config/DBconnect");
+
+const registerEmployee = async (data) => {
+  const sql = "INSERT INTO employee SET ?";
+  return new Promise((resolve, reject) => {
+    connection.query(sql, data, (error, results) => {
+      if (error) return reject(error);
+      resolve(results);
+    });
+  });
+};
+
+const getAllEmployees = async () => {
+    const sql = "SELECT * FROM employee";
+    return new Promise((resolve, reject) => {
+        connection.query(sql, (error, results) => {
+        if (error) return reject(error);
+        resolve(results);
+        });
+    }); 
+}
