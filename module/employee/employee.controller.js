1461d9c22f9dd12423096ac9226d30d9cb77cc98
MatthewOtoo <kashitokashito@gmail.com>
2025-08-18 19:42:12 +0100

    worked on the employees

--- module/employee/employee.controller.js
+++ module/employee/employee.controller.js
@@ -1,5 +1,5 @@
 const employeeService = require("./employee.service");
-
+const projectEmployeeService = require("../projectEmployee/projectEmployee_project.service");
 const getAllEmployees = async (req, res) => {
   try {
     const employees = await employeeService.getAllEmployees();
@@ -11,17 +11,62 @@ const getAllEmployees = async (req, res) => {
 
 const registerEmployee = async (req, res) => {
   try {
-    const { em_id, em_name, em_roll, em_salary, mng_id } = req.body;
-    if (!em_id || !em_name || !em_roll || !em_salary || !mng_id) {
+    const {
+      first_name,
+      last_name,
+      email,
+      phone,
+      address,
+      position,
+      base_salary,
+      hire_date,
+      project_id,
+    } = req.body;
+    if (
+      !address ||
+      !position ||
+      !base_salary ||
+      !hire_date ||
+      !project_id ||
+      !phone ||
+      !email ||
+      !last_name ||
+      !first_name
+    ) {
       return res.status(400).json({ error: "All fields are required." });
     }
-    const data = { em_id, em_name, em_roll, em_salary, mng_id };
-    // Validate the manager ID
-    const manager = await employeeService.getManagerById(mng_id);
-    if (!manager) {
-      return res.status(404).json({ error: "Manager not found." });
+    const data = {
+      first_name,
+      last_name,
+      email,
+      phone,
+      address,
+      position,
+      base_salary,
+      hire_date,
+    };
+    const result = await employeeService.createEmployee(data);
+    const employee = await employeeService.getAllEmployees();
+    const matchedEmployee = employee.find((emp) => emp.email === data.email);
+    if (!matchedEmployee) {
+      throw new Error("Employee not found after registration");
     }
-    const result = await employeeService.registerEmployee(data);
+    const employeeId = matchedEmployee.id;
+    const assignData = {
+      project_id,
+      employee_id: employeeId,
+      role: position,
+      start_date: hire_date,
+      end_date: new Date(),
+      salary_allocation: base_salary,
+    };
+
+    const details = await projectEmployeeService.assignEmployeeToProject(
+      assignData
+    );
+    if (!details) {
+      throw new Error("Failed to assign to a new project");
+    }
     res
       .status(201)
       .json({ message: "Employee registered successfully", result });
